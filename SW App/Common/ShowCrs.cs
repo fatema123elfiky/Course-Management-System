97ce99c6ee072849c2102e3cf1ab10369d0df5ae
fatema123elfiky <fatmaelfeky922@gmail.com>
2025-05-14 18:26:32 +0300

    Project organizing + Renaming some files

---
+++ SW App/Common/ShowCrs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course_Managment.Common
+{
+    public partial class ShowCrs : Form
+    {
+        public ShowCrs()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SW App/Instructor/Adds/AddStud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Course_Managment.Instructor.Adds
+{
+    public partial class AddStud : Form
+    {
+        public AddStud()
+        {
+            InitializeComponent();
+        }
+    }
+}
