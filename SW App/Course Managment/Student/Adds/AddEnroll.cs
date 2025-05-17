60f5110f1153187a6f6a392d4c6749350f3c823c
fatema123elfiky <fatmaelfeky922@gmail.com>
2025-05-17 06:33:58 +0300

    First Version

--- SW App/Course Managment/Student/Adds/AddEnroll.cs
+++ SW App/Course Managment/Student/Adds/AddEnroll.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,101 @@ namespace Course_Managment.Student.Adds
 {
     public partial class AddEnroll : Form
     {
-        public AddEnroll()
+        Student previous;
+        public AddEnroll(Student form)
         {
             InitializeComponent();
+            previous = form;
+        }
+
+        private void AddEnroll_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void View_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM ENROLL_IN WHERE SID = '" + StudIDText.Text
+                 + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, "Data Source=LAPTOP-PQTFRSSC;Initial Catalog=CrsManagement;Integrated Security=True");
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            previous.Show();
+            this.Close();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=LAPTOP-PQTFRSSC;Initial Catalog=CrsManagement;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            SqlCommand validation =new SqlCommand();
+            validation.Connection = connection;
+            cmd.Connection = connection;
+           
+            connection.Open();
+
+            ////validation for sid
+            
+            
+            if (string.IsNullOrEmpty(CrsIDText.Text.Trim()))
+            {
+                MessageBox.Show("Crs ID field is empty !!");
+                return;
+            }
+            if (string.IsNullOrEmpty(StudIDText.Text.Trim()))
+            {
+                MessageBox.Show("Student ID field is empty !!");
+                return;
+            }
+            if (string.IsNullOrEmpty(SemesterText.Text.Trim()))
+            {
+                MessageBox.Show("Semester field is empty !!");
+                return;
+            }
+            if (string.IsNullOrEmpty(YearText.Text.Trim()))
+            {
+                MessageBox.Show("Year field is empty !!");
+                return;
+            }
+
+            validation.CommandText = "SELECT COUNT(*) FROM STUDENT WHERE SID = '" + StudIDText.Text + "'";
+            int studentExists = (int)validation.ExecuteScalar();
+
+            if (studentExists == 0)
+            {
+                MessageBox.Show("Student ID does not exist.");
+                return;
+            }
+
+            validation.CommandText = "SELECT COUNT(*) FROM COURSE WHERE CID = '" + CrsIDText.Text + "'";
+            int courseExists = (int)validation.ExecuteScalar();
+
+            if (courseExists == 0)
+            {
+                MessageBox.Show("Course ID does not exist.");
+                return;
+            }
+            ////
+            
+
+            cmd.CommandText = "Insert into ENROLL_IN (SID,CRSID,SEMESTER ,YEAR ,SEMESTER) values ('" + StudIDText.Text + "','" + CrsIDText.Text + "','"
+                              + SemesterText.Text + "','" + YearText.Text + "')";
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("You enrolled now");
+
+
+
+        
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
