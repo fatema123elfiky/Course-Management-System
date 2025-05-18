cc377ade54ad2881b75aa01021d33977ca2986d9
malakmohamedrefaat <168221883+malakmohamedrefaat@users.noreply.github.com>
2025-05-18 18:44:43 +0300

    Update Login.Designer.cs

--- SW App/Course Managment/Login.Designer.cs
+++ SW App/Course Managment/Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Course_Managment
+namespace Course_Managment
 {
     partial class Login
     {
@@ -37,27 +37,32 @@
             this.emailErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.passwordErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.invalidCredsLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.emailErrorProvider)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.passwordErrorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // emailEntry
             // 
             this.emailEntry.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailEntry.Location = new System.Drawing.Point(85, 100);
+            this.emailEntry.Location = new System.Drawing.Point(364, 240);
+            this.emailEntry.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.emailEntry.MaxLength = 200;
             this.emailEntry.Name = "emailEntry";
-            this.emailEntry.Size = new System.Drawing.Size(230, 30);
+            this.emailEntry.Size = new System.Drawing.Size(258, 35);
             this.emailEntry.TabIndex = 0;
             this.emailEntry.WordWrap = false;
             // 
             // emailLabel
             // 
             this.emailLabel.AutoSize = true;
-            this.emailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailLabel.Location = new System.Drawing.Point(157, 45);
+            this.emailLabel.Font = new System.Drawing.Font("Adobe Hebrew", 18F);
+            this.emailLabel.Location = new System.Drawing.Point(116, 240);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(86, 31);
+            this.emailLabel.Size = new System.Drawing.Size(106, 45);
             this.emailLabel.TabIndex = 1;
             this.emailLabel.Text = "Email";
             this.emailLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -66,10 +71,10 @@
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordLabel.Location = new System.Drawing.Point(129, 150);
+            this.passwordLabel.Font = new System.Drawing.Font("Adobe Hebrew", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(83, 319);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(142, 31);
+            this.passwordLabel.Size = new System.Drawing.Size(162, 45);
             this.passwordLabel.TabIndex = 2;
             this.passwordLabel.Text = "Password";
             this.passwordLabel.Click += new System.EventHandler(this.passwordLabel_Click);
@@ -78,19 +83,21 @@
             // 
             this.passwordEntry.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.passwordEntry.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordEntry.Location = new System.Drawing.Point(85, 200);
+            this.passwordEntry.Location = new System.Drawing.Point(364, 319);
+            this.passwordEntry.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.passwordEntry.MaxLength = 200;
             this.passwordEntry.Name = "passwordEntry";
             this.passwordEntry.PasswordChar = '*';
-            this.passwordEntry.Size = new System.Drawing.Size(230, 30);
+            this.passwordEntry.Size = new System.Drawing.Size(258, 35);
             this.passwordEntry.TabIndex = 3;
             // 
             // loginButton
             // 
-            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(85, 290);
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.loginButton.Location = new System.Drawing.Point(259, 413);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(230, 50);
+            this.loginButton.Size = new System.Drawing.Size(188, 54);
             this.loginButton.TabIndex = 4;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = true;
@@ -109,29 +116,67 @@
             this.invalidCredsLabel.AutoSize = true;
             this.invalidCredsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.invalidCredsLabel.ForeColor = System.Drawing.Color.Red;
-            this.invalidCredsLabel.Location = new System.Drawing.Point(89, 354);
+            this.invalidCredsLabel.Location = new System.Drawing.Point(100, 442);
             this.invalidCredsLabel.Margin = new System.Windows.Forms.Padding(0);
             this.invalidCredsLabel.Name = "invalidCredsLabel";
-            this.invalidCredsLabel.Size = new System.Drawing.Size(0, 20);
+            this.invalidCredsLabel.Size = new System.Drawing.Size(0, 25);
             this.invalidCredsLabel.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Course_Managment.Properties.Resources.blended_learning;
+            this.pictureBox1.Location = new System.Drawing.Point(233, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Song Std L", 18F, System.Drawing.FontStyle.Italic);
+            this.label1.Location = new System.Drawing.Point(98, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 42);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "SMART";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Adobe Song Std L", 18F, System.Drawing.FontStyle.Italic);
+            this.label2.Location = new System.Drawing.Point(444, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 42);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "LEARNING";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(382, 393);
+            this.ClientSize = new System.Drawing.Size(749, 528);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.invalidCredsLabel);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.passwordEntry);
             this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.emailEntry);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.emailErrorProvider)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.passwordErrorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,6 +192,9 @@
         private System.Windows.Forms.ErrorProvider emailErrorProvider;
         private System.Windows.Forms.ErrorProvider passwordErrorProvider;
         private System.Windows.Forms.Label invalidCredsLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
